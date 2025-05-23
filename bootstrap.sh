f25e2d9094679ec6659cfcc830c3177aa4462ea8
iandol <iandol@cog5.org>
2025-05-23 10:26:31 +0800

    refactor file locations

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,11 @@ printf '\e[36m'
 printf "Using %s...\n" "$PLATFORM"
 printf '\e[0m'
 
-# Create bin folder
-mkdir -p $HOME/bin
+# Create folders
+mkdir -p "$HOME/bin"
+mkdir -p "$HOME/.config"
 
-# Install XCMD
+# Install X-CMD
 [[ ! -d $HOME/.x-cmd.root ]] && eval "$(curl https://get.x-cmd.com)"
 
 # Install Pixi
@@ -31,16 +32,20 @@ sudo dpkg -i $HOME/nomachine.deb
 
 # Add pixi-global.toml
 mkdir -p $HOME/.pixi/manifests
-ln -sf $SPATH/pixi-global.toml $HOME/.pixi/manifests/
+ln -sf $SPATH/config/pixi-global.toml $HOME/.pixi/manifests/
 pixi global sync
 
 # Install MATLAB
-curl -L -o ~/bin/mpm https://www.mathworks.com/mpm/glnxa64/mpm
-version="R2024a"
-products='MATLAB Curve_Fitting_Toolbox Instrument_Control_Toolbox MATLAB_Report_Generator Optimization_Toolbox Parallel_Computing_Toolbox Signal_Processing_Toolbox Statistics_and_Machine_Learning_Toolbox'
-mkdir -p "$HOME/matlab$version"
-mpath="$HOME/matlab$version"z
-$HOME/bin/mpm install --no-gpu --no-jre --release=$version --destination=$HOME/matlab$version --products=$products
+printf "Shall we use MPM to get MATLAB? [y / n]:  "
+read -r ans
+if [[ $ans == 'y' ]]; then
+	curl -L -o ~/bin/mpm https://www.mathworks.com/mpm/glnxa64/mpm
+	version="R2024a"
+	products='MATLAB Curve_Fitting_Toolbox Instrument_Control_Toolbox MATLAB_Report_Generator Optimization_Toolbox Parallel_Computing_Toolbox Signal_Processing_Toolbox Statistics_and_Machine_Learning_Toolbox'
+	mkdir -p "$HOME/matlab$version"
+	mpath="$HOME/matlab$version"z
+	$HOME/bin/mpm install --no-gpu --no-jre --release=$version --destination=$HOME/matlab$version --products=$products
+fi
 
 # APT
 if [ "$PLATFORM" = "Linux" ]; then
@@ -50,6 +55,7 @@ if [ "$PLATFORM" = "Linux" ]; then
 	sudo apt -my install openssh-server
 	sudo apt -my install p7zip-full p7zip-rar figlet jq htop 
 	sudo apt -my install libunrar5 libdc1394-25 libraw1394-11
+	sudo apt -my install gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly
 	sudo apt -my install synaptic zathura
 	sudo apt -my install snapd python3-pip
 	sudo apt -my install openjdk-17-jre
@@ -60,11 +66,11 @@ fi
 # Clone repos
 mkdir -p $HOME/Code
 cd $HOME/Code
-git clone --depth 1 https://github.com/CogPlatform/Psychtoolbox-3
-git clone --recurse-submodules https://github.com/iandol/opticka.git
-git clone --recurse-submodules https://github.com/CogPlatform/CageLab.git
-git clone --recurse-submodules https://github.com/CogPlatform/matlab-jzmq.git
-git clone --recurse-submodules https://github.com/Ccccraz/matmoteGO.git
+git clone --depth 1 https://gitee.com/CogPlatform/Psychtoolbox-3
+git clone --recurse-submodules https://gitee.com/CogPlatform/opticka.git
+git clone --recurse-submodules https://gitee.com/CogPlatform/CageLab.git
+git clone --recurse-submodules https://gitee.com/CogPlatform/matlab-jzmq.git
+git clone --recurse-submodules https://gitee.com/Ccccraz/matmoteGO.git
 
 # Setup PTB and opticka path:
 cd "$HOME/Code/Psychtoolbox-3/Psychtoolbox"
@@ -74,8 +80,14 @@ cd "$HOME"
 
 # Copy .zshrc
 [[ -e ~/.zshrc ]] && cp ~/.zshrc ~/.zshrc"$(date -Iseconds)".bak
-cp -f "$SPATH/zshrc" "$HOME/.zshrc"
-cp -f "$SPATH/aliases" "$HOME/aliases"
+cp -f "$SPATH/config/zshrc" "$HOME/.zshrc"
+cp -f "$SPATH/config/zsh-history-substring-search.zsh" "$HOME"
+cp -f "$SPATH/config/zsh-autosuggestions.zsh" "$HOME"
+cp -f "$SPATH/config/aliases" "$HOME/aliases"
+
+# few others
+cp -f "$SPATH/config/.tmux.conf" "$HOME"
+cp -f "$SPATH/config/starship.toml" "$HOME/.config/starship.toml"
 
 # switch from bash to zsh as the default shell
 if [ -x "$(which zsh)" ]; then

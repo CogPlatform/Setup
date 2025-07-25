445cb790c4fc07135f9cfbf15706a937b9d958bf
iandol <iandol@cog5.org>
2025-07-25 13:41:43 +0800

    refactor: bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,7 @@ printf "Using %s...\n" "$PLATFORM"
 printf '\e[0m'
 
 # Create some folders
+mkdir -p "$HOME/Code"
 mkdir -p "$HOME/bin"
 mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.config/systemd/user"
@@ -28,12 +29,12 @@ sudo chown -R "$USER":"$USER" /usr/local/bin
 # CHANGE name TO YOUR TOUCH PANEL NAME
 name="ILITEK-TP"
 sd '^(ExecStart=\/usr\/local\/bin\/toggleInput [^ ]+ ).*$' '$1"'$name'"' "$SPATH/config/toggleInput.service"
-sudo ln -svf "$SPATH/config/toggleInput" /usr/local/bin/toggleInput
-sudo chmod +x /usr/local/bin/toggleInput
-sudo ln -svf "$SPATH/config/toggleInput.service" "$HOME/.config/systemd/user"
-systemctl --user daemon-reload
-systemctl --user enable toggleInput.service
-systemctl --user start toggleInput.service
+sudo ln -svf "$SPATH/config/toggleInput" /usr/local/bin/toggleInput &&
+	sudo chmod +x /usr/local/bin/toggleInput &&
+	sudo ln -svf "$SPATH/config/toggleInput.service" "$HOME/.config/systemd/user" &&
+	systemctl --user daemon-reload &&
+	systemctl --user enable toggleInput.service &&
+	systemctl --user start toggleInput.service
 
 # Install X-CMD
 [[ ! -d $HOME/.x-cmd.root ]] && eval "$(curl https://get.x-cmd.com)"
@@ -54,13 +55,13 @@ fi
 printf "Shall we use MPM to get MATLAB? [y / n]:  "
 read -r ans
 if [[ $ans == 'y' ]]; then
-	curl -L -o /usr/local/bin/mpm https://www.mathworks.com/mpm/glnxa64/mpm
-	chmod +x /usr/local/bin/mpm
-	sudo mkdir -p /usr/local/MATLAB
-	sudo chown "$USER":"$USER" /usr/local/MATLAB
-	sudo chmod 777 /usr/local/MATLAB
 	version="R2025a"
 	products='MATLAB Curve_Fitting_Toolbox Instrument_Control_Toolbox MATLAB_Report_Generator Optimization_Toolbox Parallel_Computing_Toolbox Signal_Processing_Toolbox Statistics_and_Machine_Learning_Toolbox'
+	curl -L -o /usr/local/bin/mpm https://www.mathworks.com/mpm/glnxa64/mpm
+	chmod +x /usr/local/bin/mpm
+	sudo mkdir -p /usr/local/MATLAB &&
+	sudo chown "$USER":"$USER" /usr/local/MATLAB &&
+	sudo chmod 777 /usr/local/MATLAB &&
 	/usr/local/bin/mpm install --release=$version --products=$products
 fi
 
@@ -77,12 +78,11 @@ if [ "$PLATFORM" = "Linux" ]; then
 	sudo apt -my install p7zip-full p7zip-rar figlet jq htop 
 	sudo apt -my install libunrar5 libdc1394-25 libraw1394-11
 	sudo apt -my install gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly
-	sudo apt -my install synaptic zathura
+	sudo apt -my install synaptic zathura zathura-pdf-poppler zathura-ps
 	sudo apt -my install snapd python3-pip
 	sudo apt -my install openjdk-17-jre
 	sudo apt -my install flatpak
 
-	sudo snap install core arduino rpi-imager obs-studio
 	sudo snap install --classic code
 
 	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -91,13 +91,14 @@ if [ "$PLATFORM" = "Linux" ]; then
 fi
 
 # install or update cogmoteGO
-curl -sS https://raw.githubusercontent.com/Ccccraz/cogmoteGO/main/install.sh | sh
-cogmoteGO service
-cogmoteGO service start
-
+curl -sS https://raw.githubusercontent.com/Ccccraz/cogmoteGO/main/install.sh | sh &&
+	cogmoteGO service &&
+	cogmoteGO service start
 
 # Install NoMachine
-[[ ! -f /usr/NX/bin/nxd ]] && curl -o $HOME/nomachine.deb -L https://web9001.nomachine.com/download/9.0/Linux/nomachine_9.0.188_11_amd64.deb && sudo dpkg -i $HOME/nomachine.deb
+[[ ! -f /usr/NX/bin/nxd ]] && 
+	curl -o $HOME/nomachine.deb -L https://web9001.nomachine.com/download/9.0/Linux/nomachine_9.0.188_11_amd64.deb && 
+	sudo dpkg -i $HOME/nomachine.deb
 
 # Install eget and get mediamtx and sunshine
 [[ ! -f /usr/local/bin/eget ]] && curl https://zyedidia.github.io/eget.sh | sh && chmod +x eget && mv eget /usr/local/bin/eget
@@ -107,6 +108,7 @@ cogmoteGO service start
 # Clone our core repos
 mkdir -p "$HOME/Code"
 cd "$HOME/Code" || exit
+[[ ! -d 'Setup' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/Setup.git
 [[ ! -d 'Psychtoolbox' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/Psychtoolbox.git
 [[ ! -d 'opticka' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/opticka.git
 [[ ! -d 'CageLab' ]] && git clone --recurse-submodules https://gitee.com/CogPlatform/CageLab.git
@@ -117,7 +119,8 @@ cd ~ || exit
 # PTB expects libglut.so.3 but this is not present in Ubuntu 24.04 and later.
 # The following line creates a symlink to the libglut.so.3.12.0 file, which is the version available in Ubuntu 24.04 and later.
 # This allows PTB to find the library it needs to function correctly.
-[[ -f "/usr/lib/x86_64-linux-gnu/libglut.so.3.12.0" ]] && sudo ln -svf /usr/lib/x86_64-linux-gnu/libglut.so.3.12.0 /usr/lib/x86_64-linux-gnu/libglut.so.3
+[[ -f "/usr/lib/x86_64-linux-gnu/libglut.so.3.12.0" ]] && 
+	sudo ln -svf /usr/lib/x86_64-linux-gnu/libglut.so.3.12.0 /usr/lib/x86_64-linux-gnu/libglut.so.3
 
 # Setup PTB and opticka path:
 #cd "$HOME/Code/Psychtoolbox" || exit
@@ -128,7 +131,7 @@ cd "$HOME" || exit
 # Link pixi-global.toml
 rm -rf $HOME/.pixi/manifests
 mkdir -p $HOME/.pixi/manifests && ln -svf $SPATH/config/pixi-global.toml $HOME/.pixi/manifests/
-pixi global sync
+[[ -f $(which pixi) ]] && pixi global sync
 
 # Link .zshrc
 [[ -e ~/.zshrc ]] && cp ~/.zshrc ~/.zshrc"$(date -Iseconds)".bak
@@ -144,7 +147,7 @@ ln -svf "$SPATH/config/starship.toml" "$HOME/.config/starship.toml"
 # switch from bash to zsh as the default shell
 if [ -x "$(which zsh)" ]; then
 	printf 'Switching to use ZSH, you will need to reboot...\n'
-	chsh -s "$(which zsh)" && source ~/.zshrc
+	chsh -s "$(which zsh)"
 fi
 printf '\n\n--->>> All Done...\n'
 printf '\e[m'

e4df2a65712a6f883e980cea3a0c1af9aabd19c2
iandol <iandol@cog5.org>
2025-11-19 16:26:32 +0800

    feat: :sparkle:  add i3config symlink in makelinks script

--- makelinks.sh
+++ makelinks.sh
@@ -30,6 +30,7 @@ ln -svf "$SPATH/config/zsh-"* "$HOME/.config"
 ln -svf "$SPATH/config/aliases" "$HOME/.config"
 
 # few others
+ln -svf "$SPATH/config/i3config" "$HOME/.config/i3/config"
 ln -svf "$SPATH/config/Xresources" "$HOME/.Xresources"
 [[ ! -f "$HOME/.tmux.conf" ]] && ln -svf "$SPATH/config/.tmux.conf" "$HOME"
 ln -svf "$SPATH/config/cagelab-monitor.yaml" "$HOME/.config/tmuxp"
